67fc3f4be9b547ef8d0d405755b9161c2dbc4f2e
Alisa Popova <alisa.popova@gmail.com>
2020-07-09 12:27:14 +0300

    added date picker + add book panel

--- js/app.js
+++ js/app.js
@@ -1,37 +1,67 @@
 
 let BOOKS_LIST_CONTAINER;
 
-const boorRowTeplate = function(book) {
+const bookRowTeplate = function(book) {
     return `<div class="row">
-        <div class="col s2">${book.num}</div>
-        <div class="col s2">${book.title}</div>
-        <div class="col s2">${book.author}</div>
-        <div class="col s2">${book.year}</div>
-        <div class="col s2">
         <!-- Switch -->
-              <div class="switch">
-                    <label><input type="checkbox" ${book.finished == true ? 'checked' : ''}><span class="lever"></span></label>
-              </div>
+        <div class="col s1 switchCol">
+            <label>
+                <input type="checkbox" class="filled-in switcher" ${book.finished == true ? 'checked' : ''} />
+                <span></span>
+             </label>
         </div>
+        <div class="col s1">${format(book.num)}</div>
+        <div class="col s5">${format(book.title)}</div>
+        <div class="col s3">${format(book.author)}</div>  
+        <div class="col s2">${format(book.dateFinished)}</div>
     </div>`;
 };
 
 init();
+
+function format(value) {
+    return typeof value == 'undefined' || value == null ? '' : value;
+}
 
 function init() {
     document.addEventListener("DOMContentLoaded", load());
 }
 
 
+function onBookRowClick(e) {
+    console.log(`onBookRowClick: e.target `, e.target);
+    let el =  e.target;
+    if (el.classList.contains("switcher")) {
+        console.log('k', el.parentElement.parentElement.parentElement.children[4])
+        el.parentElement.parentElement.parentElement.children[4].innerHTML = new Date().toLocaleDateString();
+        //TODO update model
+    }
+
+}
+
 function load() {
     BOOKS_LIST_CONTAINER =  document.getElementById("booksList");
+    BOOKS_LIST_CONTAINER.addEventListener("click", onBookRowClick);
+
+    document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.datepicker');
+        var currYear = (new Date()).getFullYear();
+        var instances = M.Datepicker.init(elems, {
+            defaultDate: Date.now(),
+            setDefaultDate: Date.now(),
+            yearRange: [currYear - 5, currYear + 1],
+            format: "dd-mm-yyyy"
+        });
+    });
+
     fetch("js/books.json")
         .then(response => response.json())
         .then(books => {
 
             books.forEach(book => {
+
                 let row = document.createElement('div');
-                row.innerHTML = boorRowTeplate(book);
+                row.innerHTML = bookRowTeplate(book);
                 BOOKS_LIST_CONTAINER.appendChild(row);
             })
         });
